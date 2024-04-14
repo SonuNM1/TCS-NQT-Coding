2526c3e57d855cc403c7a85b9139b9cd7089add7
Sonu N Mahto <sonu.mahto362000@gmail.com>
2024-04-14 12:19:54 +0530

    14th April

---
+++ PrepInsta-Youtube/question12.java
@@ -0,0 +1,25 @@
+
+/* 
+
+The program will receive 3 English words inputs from STDIN
+
+- These three words will be read one at a time, in three separate line
+
+- The first word should be changed like all vowels should be replaced by % 
+
+- The second word should be changed like all consonants should be replaced by #
+
+- The third word should be changed like all char should be converted to uppercase 
+
+- then concatentate the three words and print them 
+
+Input: how are you 
+Output: h%wa#eYOU
+
+*/
+
+public class question12{
+    public static void main(String args[]){
+
+    }
+}
